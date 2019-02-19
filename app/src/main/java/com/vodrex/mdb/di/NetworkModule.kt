26846799ca4db865e4852babd615eabdc10601bb
vodre <vodrex@gmail.com>
2019-02-19 16:28:44 -0600

    Initial Commit

--- app/src/main/java/com/vodrex/mdb/di/NetworkModule.kt
+++ app/src/main/java/com/vodrex/mdb/di/NetworkModule.kt
@@ -19,9 +19,7 @@ import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
-/**
- * Created by ebayhan on 22.01.2019.
- */
+
 object NetworkModule {
     fun getRemote(): AppService {
         return Retrofit.Builder()
